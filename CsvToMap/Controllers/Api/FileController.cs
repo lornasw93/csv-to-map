96ef8e5aabbe1d3d820ebdebb54e1f3b0e5236f0
lornasw93 <lornnaa@live.com>
2019-10-24 15:00:42 +0100

    Add project files.

---
+++ CsvToMap/Controllers/Api/FileController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using CsvToMap.Models;
+using MarkEmbling.PostcodesIO;
+using Newtonsoft.Json;
+
+namespace CsvToMap.Controllers.Api
+{
+    public class FileController : ApiController
+    {
+        private const string Path = "C:\\Users\\lorna.watson\\Documents\\Projects\\CsvToMap";
+
+        [System.Web.Mvc.HttpGet]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var list = new List<Map>();
+                var data = File.ReadAllLines($"{Path}\\LatitudeLongitude.csv");
+
+                list = data.Skip(1)
+                    .Select(t => t.Split(','))
+                    .Select(split => new Map
+                    {
+                        Latitude = Convert.ToDouble(split[0]),
+                        Longitude = Convert.ToDouble(split[1])
+                    }).ToList();
+
+                var postcodeList = GetPostcodes();
+                var count = postcodeList.Count() / 100;
+                var chunks = ChunkBy(postcodeList.ToList(), count);
+                var client = new PostcodesIOClient();
+
+                foreach (var chunk in chunks)
+                {
+                    var bulkResult = client.BulkLookup(chunk);
+                    list.AddRange(from item in bulkResult
+                                  where item.Result != null
+                                  select new Map
+                                  {
+                                      Latitude = Convert.ToDouble(item.Result.Latitude),
+                                      Longitude = Convert.ToDouble(item.Result.Longitude)
+                                  });
+                }
+
+                // write to file - results.json
+                SerializeToFile(list);
+
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private static List<string> GetPostcodes()
+        {
+            var data = File.ReadAllLines($"{Path}\\Postcodes.csv");
+            var postcodes = data.Skip(1)
+                .Select(c => !string.IsNullOrWhiteSpace(c.Split(',')[0])
+                    ? c.Split(',')[0]
+                    : string.Empty);
+            return postcodes.ToList();
+        }
+
+        private static void SerializeToFile(List<Map> list)
+        {
+            using (var stream = File.CreateText($"{Path}\\Results.json"))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(stream, list);
+            }
+        }
+
+        private static List<List<T>> ChunkBy<T>(IEnumerable<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+
+        private bool CsvExists()
+        {
+            var directory = new DirectoryInfo(Path);
+            if (!directory.Exists)
+                return false;
+
+            var files = directory.GetFiles().OrderByDescending(f => f.LastWriteTime);
+            return (files.Any());
+        }
+    }
+}
---
+++ CsvToMap/Controllers/Api/MapController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using CsvToMap.Models;
+using MarkEmbling.PostcodesIO;
+using Newtonsoft.Json;
+
+namespace CsvToMap.Controllers.Api
+{
+    public class MapController : ApiController
+    {
+        private const string Path = "C:\\Users\\lorna.watson\\Documents\\Projects\\CsvToMap";
+
+        [System.Web.Mvc.HttpGet]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var list = new List<Map>();
+
+                var directory = new DirectoryInfo(Path);
+                if (directory.Exists)
+                {
+                    var files = directory.GetFiles().OrderByDescending(f => f.LastWriteTime);
+                    if (files.Any())
+                    {
+                        var file = files.FirstOrDefault().FullName;
+
+                        var json = File.ReadAllText(file);
+                        list = JsonConvert.DeserializeObject<List<Map>>(json);
+                    }
+                }
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
